3dd6ac73ba17df23e28394d463a18e8aef2577ce
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-20 14:23:30 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,6 +1,16 @@
 
 git pull
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule update -f --merge
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git submodule foreach "git pull"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
@@ -13,11 +23,28 @@ msbuild /t:BuildAll  Solution.build
 
 :End
 git add --all
-git commit -m"Update docs" --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git commit -m"Auto Version Update" --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 
 git submodule foreach "git add --all"
-git submodule foreach "git commit -m'Auto Update SubModules'-a"
-git submodule foreach "git push --all"
-git push --all --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git submodule foreach "git commit -ma'Auto Update SubModules'"
+# set GIT_STATUS=%ERRORLEVEL% 
+# if not %GIT_STATUS%==0 goto fail 
+
+git submodule foreach "git push -f origin HEAD:master"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git push -f --all --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 git request-pull master https://github.com/StarShip-Avalon-Projects/MonkeySpeakExtendedEngine.git
