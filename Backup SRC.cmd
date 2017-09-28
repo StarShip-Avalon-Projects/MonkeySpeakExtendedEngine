647e993314ae3dbfaf0b439b2bc628f787f460bc
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-28 14:40:29 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,13 +1,4 @@
-
-git pull
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
-
-git submodule update -f --merge
-
-git submodule foreach "git pull"
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
+call UpdateSrc.cmd
 
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
@@ -40,20 +31,19 @@ git add --all
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
 
-git commit -m"Auto Version Update" --all
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
-
-
 git submodule foreach "git add --all"
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
 
-git submodule foreach "git commit -ma'Auto Update SubModules'"
+git submodule foreach "git commit -m'Auto Update SubModules'"
 
-git push --all --recurse-submodules=on-demand
+git commit -m"Auto Version Update"
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
+
+git push --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto eof
 
 git request-pull master https://github.com/StarShip-Avalon-Projects/MonkeySpeakExtendedEngine.git
 
