0afc6e9d94799ec1182e9ec9878a2b085e63c94c
Jeremy Trombley <gerolkae@hotmail.com>
2017-10-13 09:32:27 -0700

    Auto Version Update

--- Backup.cmd
+++ Backup.cmd
@@ -1,12 +1,8 @@
-call UpdateSrc.cmd
-set BUILD_STATUS=%ERRORLEVEL% 
-if not %BUILD_STATUS%==0 goto fail 
 
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
-
 msbuild /t:IncrementVersions  Solution.build
 set BUILD_STATUS=%ERRORLEVEL% 
 if not %BUILD_STATUS%==0 goto fail 
@@ -14,15 +10,14 @@ if not %BUILD_STATUS%==0 goto fail
 :BuildAll
 msbuild /t:BuildAll  Solution.build
 set BUILD_STATUS=%ERRORLEVEL% 
-if %BUILD_STATUS%==0 goto BuildRelease 
 if not %BUILD_STATUS%==0 goto fail 
 
 :BuildRelease
- msbuild /t:Build /property:Configuration=Release Solution.build
+msbuild /t:Build /property:Configuration=Release Solution.build
  set BUILD_STATUS=%ERRORLEVEL% 
 if %BUILD_STATUS%==0 goto end 
-if not %BUILD_STATUS%==0 goto fail 
-
+if not %BUILD_STATUS%==0 goto fail  
+ 
 :fail 
 pause 
 exit /b 1
--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -1,16 +1,7 @@
 
-git pull
+git fetch --recurse-submodules=on-demand
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto fail 
-
-git submodule update --merge
-
-git submodule foreach "git pull"
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail
-
-
-
 
 :eof
 exit /b 0
